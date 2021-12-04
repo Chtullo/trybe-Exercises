06a7bb4f7decf1fbbdcb00c14f90b7d12c82adac
Nicholas Castro <contabrpt2@hotmail.com>
2021-12-04 16:32:21 -0300

    exercicios de fixação do course

---
+++ scripts.js
@@ -0,0 +1,36 @@
+// // const base = 5;/* 
+// const height = 8;
+// const area = (base*height);
+// console.log (area);
+// const perimeter = (base*2 +height*2)
+// console.log (perimeter);
+//  */
+// const nota = 300;
+// if (nota >= 80){
+//     console.log("Parabéns, você foi aprovada(o)!")
+// }
+// else if (nota <80 && nota >= 60){
+//     console.log("Você está na lista de espera")
+// }
+// else if (nota <60){
+//     console.log ("Você foi reprovado")
+// }
+// const currentHour = 19;
+// let message;
+//    if (currentHour >= 22){
+//     message = "Não deveríamos comer nada, é hora de dormir"
+// } else if (currentHour >= 18 && currentHour < 22){
+//     message = "Rango da noite, vamos jantar :D"
+// } else if (currentHour >= 14 && currentHour < 18){
+//     message = "Vamos fazer um bolo pro café da tarde ?"
+// } else if (currentHour >= 11 && currentHour < 14){
+//     message = "Hora do almoço!!!"
+// } else if (currentHour >= 4 && currentHour < 11){
+//     message = "Hmmmm, cheiro de café recém passado"
+// } console.log(message)
+let weekDay ="dominguera"
+if (weekDay == "segunda-feira"||"terça-feira"||"quarta-feira"||"quinta-feira"||"sexta-feira"){
+    console.log ("Oba, mais um dia de aprendizado na Trybe :D")
+}else if (weekDay == "sábadão"||"dominguera"){
+    console.log ("FINALMENTE, descanso merecido UwU")
+}
